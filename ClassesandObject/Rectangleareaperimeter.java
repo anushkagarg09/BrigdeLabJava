02f422a43e1354744f9eb7bd723b0d11d664439a
anushkagarg09 <anushkagarg751@gmail.com>
2025-08-13 11:41:54 +0530

    Update Rectangleareaperimeter.java

--- ClassesandObject/Rectangleareaperimeter.java
+++ ClassesandObject/Rectangleareaperimeter.java
@@ -1,6 +1,19 @@
-class LocalVarError {
+class Rectangle {
+    int length, width;
+
+    Rectangle(int l, int w) {
+        length = l;
+        width = w;
+    }
+
+    void calculate() {
+        int area = length * width;
+        int perimeter = 2 * (length + width);
+        System.out.println("Area: " + area + ", Perimeter: " + perimeter);
+    }
+
     public static void main(String[] args) {
-        int x;
-        System.out.println(x); 
+        Rectangle r = new Rectangle(5, 3);
+        r.calculate();
     }
 }
