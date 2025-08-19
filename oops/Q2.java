88ca71e9d2bd02673bacb4da011d0757cbf5a9af
anushkagarg09 <anushkagarg751@gmail.com>
2025-08-19 10:19:56 +0530

    Update Q2.java

--- oops/Q2.java
+++ oops/Q2.java
@@ -1,1 +1,30 @@
+package bridgeLabz.oops;
+class cartest{
+String model;
+int year;
 
+    static int numberOfCars=0;
+
+    cartest(String model, int year){
+        this.model=model;
+        this.year=year;
+        numberOfCars++;
+    }
+
+    void displayDetails(){
+        System.out.println("Model: "+model+", Year: "+year);
+    }
+
+    
+
+    public static void main(String[]args){
+        cartest car1=new cartest("Tata", 2017);
+        cartest car2=new cartest("Audi", 2009);
+
+        car1.displayDetails();
+        car2.displayDetails();
+
+        System.out.print(numberOfCars);
+    }
+
+}
