f805ba23b9078ca3bfb3e4b0782793bb06cd3999
anushkagarg09 <anushkagarg751@gmail.com>
2025-08-19 10:20:19 +0530

    Update Q3.java

--- oops/Q3.java
+++ oops/Q3.java
@@ -1,1 +1,13 @@
+package bridgeLabz.oops;
+class method{
 
+    void show(){
+        int num=23;
+       System.out.print(num);
+    }
+    public static void main(String[]args){
+        //System.out.print(num);
+        // method obj=new method();
+        // obj.show();
+    }
+}
