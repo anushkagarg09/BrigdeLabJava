22866606d419b6475086cd4610c683a9921da986
anushkagarg09 <anushkagarg751@gmail.com>
2025-08-19 10:19:31 +0530

    Update Q1.java

--- oops/Q1.java
+++ oops/Q1.java
@@ -1,1 +1,28 @@
+package bridgeLabz.oops;
+class car{
+String model;
+int year;
 
+    static int numberOfCars=0;
+
+    car(String model, int year){
+        this.model=model;
+        this.year=year;
+        numberOfCars++;
+    }
+
+    void displayDetails(){
+        System.out.println("Model: "+model+", Year: "+year);
+    }
+
+    
+
+    public static void main(String[]args){
+        car car1=new car("Tata", 2017);
+        car car2=new car("Audi", 2009);
+
+        car1.displayDetails();
+        car2.displayDetails();
+    }
+
+}
