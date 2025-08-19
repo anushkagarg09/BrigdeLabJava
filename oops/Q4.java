27f7b11045ed6ced173679fdc6348dbea3ad2ef3
anushkagarg09 <anushkagarg751@gmail.com>
2025-08-19 10:24:42 +0530

    Update Q4.java

--- oops/Q4.java
+++ oops/Q4.java
@@ -1,1 +1,28 @@
+class Student {
+    // Attributes
+    String name;
+    int marks;
+
+    // Constructor to initialize values
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    // Method to display student data
+    void display() {
+        System.out.println("Name: " + name + ", Marks: " + marks);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Creating two objects of Student
+        Student s1 = new Student("Anushka", 90);
+        Student s2 = new Student("Rahul", 85);
+
+        // Printing their data
+        s1.display();
+        s2.display();
+    }
+}
 
