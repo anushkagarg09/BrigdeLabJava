5e32a1c0a2d997dc1f2d27d1840c3d29b652d0a8
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-09 08:11:24 +0530

    Update DateArithmetic.java

--- BuiltInFun/DateArithmetic.java
+++ BuiltInFun/DateArithmetic.java
@@ -1,1 +1,16 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
+public class DateArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime ist = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pst = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("GMT: " + gmt.toLocalTime());
+        System.out.println("IST: " + ist.toLocalTime());
+        System.out.println("PST: " + pst.toLocalTime());
+
+    }
+}
