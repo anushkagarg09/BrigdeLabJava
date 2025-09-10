d2d994e4f82b131a626985b5433574719c178fc0
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:35:59 +0530

    Update Ques2.java

--- JavaConstructor/Ques2.java
+++ JavaConstructor/Ques2.java
@@ -1,1 +1,24 @@
+package javaConstructor;
+
+class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println("Product Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class Ques2 {
+	  public static void main(String[] args) {
+	        Product p = new Product("Laptop", 75000.0);
+	        p.display();
+	    }
+}
 
