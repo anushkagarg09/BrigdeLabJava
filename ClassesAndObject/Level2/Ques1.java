e47046573fa263b0b5c93a571be4d782a889b1e0
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:50:25 +0530

    Ques1.java

--- ClassesAndObject/Level2/Ques1.java
+++ ClassesAndObject/Level2/Ques1.java
@@ -1,1 +1,37 @@
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
 
+   
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+
+    String calculateGrade() {
+        if (marks >= 90) return "A+";
+        else if (marks >= 80) return "A";
+        else if (marks >= 70) return "B";
+        else if (marks >= 60) return "C";
+        else if (marks >= 50) return "D";
+        else return "F";
+    }
+
+  
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+}
+
+public class StudentReport {
+    public static void main(String[] args) {
+        Student student = new Student("Komal Patel", 101, 85);
+        student.displayDetails();
+    }
+}
