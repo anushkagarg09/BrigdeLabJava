68c943d8a35a725c3e2aa605375f20c8378015a9
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:51:55 +0530

    Ques4.java

--- ClassesAndObject/Level2/Ques4.java
+++ ClassesAndObject/Level2/Ques4.java
@@ -1,1 +1,34 @@
+class MovieTicket {
+    String movieName;
+    String seatNumber;
+    double price;
 
+
+    MovieTicket(String movieName) {
+        this.movieName = movieName;
+        this.seatNumber = "";
+        this.price = 0;
+    }
+
+
+    void bookTicket(String seatNumber, double price) {
+        this.seatNumber = seatNumber;
+        this.price = price;
+        System.out.println("Ticket booked successfully!");
+    }
+
+
+    void displayTicket() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: $" + price);
+    }
+}
+
+public class MovieBooking {
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket("Avengers: Endgame");
+        ticket.bookTicket("A10", 12.50);
+        ticket.displayTicket();
+    }
+}
