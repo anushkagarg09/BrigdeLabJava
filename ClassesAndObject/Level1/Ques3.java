f4a98c66e1efbebc73f559c67a06c3d77ceed594
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:46:42 +0530

    Ques3.java

--- ClassesAndObject/Level1/Ques3.java
+++ ClassesAndObject/Level1/Ques3.java
@@ -1,1 +1,26 @@
+class Book {
+    String title;
+    String author;
+    double price;
 
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+  
+    void displayDetails() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+    }
+}
+
+public class BookDetails {
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "Komal Patel", 450.50);
+        book.displayDetails();
+    }
+}
