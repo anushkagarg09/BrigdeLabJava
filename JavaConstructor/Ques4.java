6b2a00870aef2b36c293a3c237bb2d58aec785f2
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:37:11 +0530

    Update Ques4.java

--- JavaConstructor/Ques4.java
+++ JavaConstructor/Ques4.java
@@ -1,1 +1,31 @@
+package javaConstructor;
+
+class P {
+    private String name;
+    private double price;
+    private double discountedPrice;
+
+    public  P(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.discountedPrice = calculatePrice(price); 
+    }
+
+    private double calculatePrice(double originalPrice) {
+        return originalPrice * 0.9;
+    }
+
+    public void display() {
+        System.out.println("Product Name: " + name);
+        System.out.println("Original Price: " + price);
+        System.out.println("Discounted Price: " + discountedPrice);
+    }
+}
+
+public class Ques4 {
+    public static void main(String[] args) {
+        P p1 = new P(null, 0);
+        p1.display();
+    }
+}
 
