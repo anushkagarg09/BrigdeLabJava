45989ecab01da3af012fb374a6d16a59f7fcdede
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:50:53 +0530

    Ques2.java

--- ClassesAndObject/Level2/Ques2.java
+++ ClassesAndObject/Level2/Ques2.java
@@ -1,1 +1,43 @@
+class BankAccount {
+    String accountHolder;
+    String accountNumber;
+    double balance;
 
+    
+    BankAccount(String accountHolder, String accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("$" + amount + " deposited successfully.");
+    }
+
+    
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("$" + amount + " withdrawn successfully.");
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+   
+    void displayBalance() {
+        System.out.println("Current balance: $" + balance);
+    }
+}
+
+public class ATM {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("Komal Patel", "ACC123", 1000);
+        account.displayBalance();
+        account.deposit(500);
+        account.withdraw(300);
+        account.displayBalance();
+    }
+}
