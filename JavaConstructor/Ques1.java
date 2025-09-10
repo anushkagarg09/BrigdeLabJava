433df1a20cc1ce0df84c1465d7cc08a0b92ebf14
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:35:21 +0530

    Update Ques1.java

--- JavaConstructor/Ques1.java
+++ JavaConstructor/Ques1.java
@@ -1,1 +1,22 @@
+package javaConstructor;
+
+class Customer {
+    private String customerName;
+
+    public Customer(String customerName) {
+        this.customerName = customerName; 
+    }
+
+    public void display() {
+        System.out.println("Customer Name: " + customerName);
+    }
+}
+
+public class Ques1 {
+	public static void main(String[] args) {
+        Customer c = new Customer("Anushka");
+        c.display();
+    }
+
+}
 
