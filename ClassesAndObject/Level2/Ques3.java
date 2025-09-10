09dc9f434760bf2e44253a4d03258458619816a4
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-10 10:51:22 +0530

    Ques3.java

--- ClassesAndObject/Level2/Ques3.java
+++ ClassesAndObject/Level2/Ques3.java
@@ -1,1 +1,36 @@
+class PalindromeChecker {
+    String text;
 
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+   
+    boolean isPalindrome() {
+        String cleanText = text.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = cleanText.length() - 1;
+        while (left < right) {
+            if (cleanText.charAt(left) != cleanText.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+   
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+}
+
+public class PalindromeProgram {
+    public static void main(String[] args) {
+        PalindromeChecker pc = new PalindromeChecker("Madam");
+        pc.displayResult();
+    }
+}
