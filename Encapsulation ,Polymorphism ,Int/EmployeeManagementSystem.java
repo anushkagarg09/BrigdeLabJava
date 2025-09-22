209c97dfe4ce35bb26adf0b005a6745a3d6d5800
anushkagarg09 <anushkagarg751@gmail.com>
2025-09-22 19:59:57 +0530

    EmployeeManagementSystem.java

---
+++ Encapsulation ,Polymorphism ,Int/EmployeeManagementSystem.java
@@ -0,0 +1,126 @@
+package ProblemStatements;
+import java.util.*;
+
+abstract class Employee {
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateSalary();
+
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId + ", Name: " + name + ", Base Salary: " + baseSalary + ", Final Salary: " + calculateSalary());
+    }
+}
+
+interface Department {
+    void assignDepartment(String dept);
+    String getDepartmentDetails();
+}
+
+class FullTimeEmployee extends Employee implements Department {
+    private String department;
+
+    public FullTimeEmployee(int employeeId, String name, double baseSalary) {
+        super(employeeId, name, baseSalary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary();
+    }
+
+    @Override
+    public void assignDepartment(String dept) {
+        this.department = dept;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Department: " + getDepartmentDetails());
+    }
+}
+
+class PartTimeEmployee extends Employee implements Department {
+    private int hoursWorked;
+    private double hourlyRate;
+    private String department;
+
+    public PartTimeEmployee(int employeeId, String name, double hourlyRate, int hoursWorked) {
+        super(employeeId, name, 0);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+
+    @Override
+    public void assignDepartment(String dept) {
+        this.department = dept;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Department: " + getDepartmentDetails());
+    }
+}
+
+public class EmployeeManagementSystem {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+
+        FullTimeEmployee f1 = new FullTimeEmployee(1, "Mahi", 50000);
+        f1.assignDepartment("HR");
+
+        PartTimeEmployee p1 = new PartTimeEmployee(2, "Anushka", 200, 40);
+        p1.assignDepartment("IT");
+
+        employees.add(f1);
+        employees.add(p1);
+
+        for (Employee emp : employees) {
+            emp.displayDetails();
+            System.out.println("-----------");
+        }
+    }
+}
+
+
+
